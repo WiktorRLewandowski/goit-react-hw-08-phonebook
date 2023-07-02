aafead18af49bc48c2fcb6c03d112ff33fb48e24
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-07-02 15:55:03 +0200

    it could be done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,15 @@ const INITIAL_STATE = {
   filter: '',
 }
 
+const INITIAL_CONTACTS = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+}
+
 class App extends Component {
   state = {
     contacts: [
@@ -27,6 +36,21 @@ class App extends Component {
     name: PropTypes.string,
     number: PropTypes.string,
     filter: PropTypes.string
+  }
+  
+  componentDidMount() {
+    const localStorageItems = JSON.parse(localStorage.getItem('contacts'))
+    if (!localStorageItems[0]) return this.setState({...INITIAL_STATE, ...INITIAL_CONTACTS})
+    this.setState({contacts: localStorageItems})
+  }
+
+  componentDidUpdate() {
+    this.localStorageSaver()
+  } // i feel like this should be in ContactList, but maybe we can get it with hooks later on
+
+  localStorageSaver() {
+    const {contacts} = this.state
+    localStorage.setItem("contacts", JSON.stringify(contacts));
   }
 
   handleSubmit = (ev) => {
