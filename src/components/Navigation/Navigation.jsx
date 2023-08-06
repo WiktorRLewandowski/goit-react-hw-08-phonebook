1983faf98354816a528c6be8f1ea4f557c68cfd7
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-08-06 20:32:35 +0200

    bugfix/nav-container

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,18 +10,21 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Loader } from 'components/Loader';
 import { UserMenu } from 'components/UserMenu';
+import { Container } from '@mui/material';
 
 export default function ButtonAppBar() {
   return (
     <>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
+        <Container maxWidth='lg'>
         <Toolbar>          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Phonebook
           </Typography>
           <UserMenu/>
         </Toolbar>
+        </Container>
       </AppBar>
     </Box>
     <Suspense fallback={<Loader/>}>
